2775d0a8a88499cdcb49bd84beaf92a295f6237d
Moore <derekmoore@derek-jr-jr.metagenics.com>
2018-11-20 16:44:52 -0800

    reset is working. Full game functionality is working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,11 +11,57 @@ $(document).ready(function () {
         var lossCount = 0;
         //kitten values needs to be an empty array so I can randomly append to it.
         kittenValues = [];
-
+        $("#winCount").text(winCount);
         $("#numberToGuess").text(goalNumber);
-
+        $("#lossCount").text(lossCount);
 
         // generate the 4 numbers for the kittens
+        setNumbers();
+
+        // Click function that adds the numbers and compares them to each other.
+        $(".kitten-button").on("click", function () {
+            var kittenCounter = ($(this).attr("data-number"));
+            kittenCounter = parseInt(kittenCounter);
+            currentGuessCounter += kittenCounter;
+            $("#currentNumber").text(currentGuessCounter);
+
+
+
+            //win / loss logic
+            //======================================================
+            if (currentGuessCounter === goalNumber) {
+                winCount++;
+
+                function delay() {
+                    alert("YOU WON!! Math must be your favorite subject!");
+                }
+                reset();
+                setTimeout(delay, 300);
+            } else if (currentGuessCounter > goalNumber) {
+                lossCount++;
+                function delay() {
+                    alert("You lost :( You should Study a little more");
+                }
+                setTimeout(delay, 300);
+                reset();
+            }
+        });
+
+        // Reset function after game is over
+        function reset() {
+            currentGuessCounter = 0;
+            goalNumber = Math.floor(Math.random() * 100) + 20;
+            setNumbers();
+            kittenValues = [];
+            $("#winCount").text(winCount);
+            $("#numberToGuess").text(goalNumber);
+            $("#lossCount").text(lossCount);
+            $("#currentNumber").text("00");
+        }
+
+    }
+    // I had to use this 2 times, so I decided to make it a function
+    function setNumbers() {
         for (i = 0; i < 4; i++) {
             var init = (Math.floor(Math.random() * (15 + 1)));
             // making sure that the numbers are not duplicated
@@ -28,55 +74,11 @@ $(document).ready(function () {
                 i--;
             }
         }
-
-        $(".kitten-button").on("click", function () {
-
-
-            var kittenCounter = ($(this).attr("data-number"));
-            kittenCounter = parseInt(kittenCounter);
-
-            currentGuessCounter += kittenCounter;
-            $("#currentNumber").text(currentGuessCounter);
-            console.log(currentGuessCounter);
-
-
-//win / loss logic
-
-if (currentGuessCounter === goalNumber) {
-    winCount++;
-
-
-    function delay() {
-        alert("YOU WON!! Math must be your favorite subject!");
-
     }
 
-    setTimeout(delay, 300);
-} else if (currentGuessCounter > goalNumber) {
-    lossCount++;
-
-
-    function delay() {
-        alert("You lost :( You should Study a little more");
-
-    }
-
-    setTimeout(delay, 300);
-
-}
-
-        });
-
-        
-
-        // Reset function after game is over
 
 
 
-        console.log(kittenValues);
-        console.log(currentGuessCounter);
-
-    }
     startGame();
 
 
